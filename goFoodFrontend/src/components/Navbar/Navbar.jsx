eecaf3b6e7cceccd01e154929650129058eb82ef
Aastha0625 <justofficial06@gmail.com>
2025-06-12 23:14:18 +0530

    Setting up

---
+++ goFoodFrontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <img src = {}
+            </div>
+    );
+}
+
+export default Navbar
