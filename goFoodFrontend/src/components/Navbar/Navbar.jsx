d36d2e5b9a2c37a6941b6eb4a423733568f8a7c9
Aastha0625 <justofficial06@gmail.com>
2025-06-19 09:43:08 +0530

    Signup/Login done

--- goFoodFrontend/src/components/Navbar/Navbar.jsx
+++ goFoodFrontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets';
+import {Link} from 'react-router-dom';
 
-function Navbar() {
+const Navbar = ({setShowLogin})=> {
 
     const [menu,setMenu] = useState("Home");
     return (
         <div className='navbar'>
             <img src={assets.logo} alt="" className='logo' />
             <ul className='navbar-menu'>
-                <li on onClick={()=>setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home </li >
-                <li on onClick={()=>setMenu("Menu")} className={menu === "Menu" ? "active": ""}>Menu </li>
-                <li on onClick={()=>setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active": ""}>Mobile-app </li>
-                <li on onClick={()=>setMenu("Contact us")} className={menu === "Contact us" ? "active": ""}>Contact us </li>
+                <Link to='/' onClick={()=>setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</Link> 
+                <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu === "Menu" ? "active": ""}>Menu </a>
+                <a href='#app-download' onClick={()=>setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active": ""}>Mobile-app </a>
+                <a href='#footer' onClick={()=>setMenu("Contact us")} className={menu === "Contact us" ? "active": ""}>Contact us </a>
             </ul>
             <div className='navbar-right'>
                 <img src={assets.search_icon} alt='' />
@@ -20,10 +21,10 @@ function Navbar() {
                     <img src={assets.basket_icon} alt="" />
                     <div className='dot'></div>
                 </div>
-                <button>Sign In</button>
+                <button onClick={()=>setShowLogin(true)}>Sign In</button>
             </div>
             </div>
     );
 }
 
-export default Navbar
+export default Navbar 
